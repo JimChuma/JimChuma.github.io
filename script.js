e92181429e2e8146021daa14a6be138f83a171bf
JimChuma <85954599+JimChuma@users.noreply.github.com>
2024-02-27 18:58:05 +0100

    Project-addition performance test

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function toggleMenu() {
     menu.classList.toggle('open');
     icon.classList.toggle('open');
 }
+
+let today = new Date();
+let thisYear = today.getFullYear();
+let footerText = document.querySelector('footer p');
+footerText.textContent = 'Copyright © ' + thisYear + ' Jim Oputa. All Rights Reserved.';
